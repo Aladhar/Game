e6c464a141e6ae85e38c7d36065e7480a7fb8371
Amrit Ladhar <67712683+Aladhar@users.noreply.github.com>
2026-05-29 14:32:49 -0700

    Add EnhancedInput module dependency

--- Penance/Source/PenanceDemoUE/PenanceDemoUE.Build.cs
+++ Penance/Source/PenanceDemoUE/PenanceDemoUE.Build.cs
@@ -11,6 +11,7 @@ public class PenanceDemoUE : ModuleRules
             "Core",
             "CoreUObject",
             "Engine",
+            "EnhancedInput",
             "InputCore"
         });
     }
